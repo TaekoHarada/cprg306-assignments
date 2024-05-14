3534609a3ce7b2ebb5cc2fea16738f6302fb514f
TaekoHarada <taecooo512@gmail.com>
2024-05-13 19:58:58 -0600

    Update

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Student Info</h1>
+      <div>Taeko Harada</div>
+      <Link href="https://github.com/TaekoHarada">Git Hub</Link>
+    </main>
+  );
+}
